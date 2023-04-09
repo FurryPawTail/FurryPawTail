ffe2305562544b050587d6d75d1428f73c8014c4
Mike <wl02599509@gmail.com>
2023-04-10 00:07:51 +0800

    run rubocop -A

--- app/models/advanced_user.rb
+++ app/models/advanced_user.rb
@@ -1,2 +1,4 @@
+# frozen_string_literal: true
+
 class AdvancedUser < ApplicationRecord
 end
--- db/migrate/20230325070352_create_matchmakers.rb
+++ db/migrate/20230325070352_create_matchmakers.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class CreateMatchmakers < ActiveRecord::Migration[7.0]
   def change
     create_table :matchmakers do |t|
