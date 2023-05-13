266ccaf40fd33fe812a15e435f7ab61f15d223d0
Mike <wl02599509@gmail.com>
2023-05-13 23:47:53 +0800

    migration and schema

--- db/schema.rb
+++ db/schema.rb
@@ -12,7 +12,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 20_230_325_070_352) do
+ActiveRecord::Schema[7.0].define(version: 20_230_513_153_857) do
   create_table 'adoptees', charset: 'utf8mb4', collation: 'utf8mb4_0900_ai_ci', force: :cascade do |t|
     t.string 'name', null: false
     t.string 'species'
@@ -96,5 +96,12 @@ ActiveRecord::Schema[7.0].define(version: 20_230_325_070_352) do
     t.string 'gender'
     t.datetime 'created_at', null: false
     t.datetime 'updated_at', null: false
+    t.string 'email', default: '', null: false
+    t.string 'encrypted_password', default: '', null: false
+    t.string 'reset_password_token'
+    t.datetime 'reset_password_sent_at'
+    t.datetime 'remember_created_at'
+    t.index ['email'], name: 'index_users_on_email', unique: true
+    t.index ['reset_password_token'], name: 'index_users_on_reset_password_token', unique: true
   end
 end
