a1bf1b03fe1bfdb443cd6b6afd11c16a7e004c5d
Mike <wl02599509@gmail.com>
2023-10-07 17:45:51 +0800

    compile from ts

---
+++ dist/components/index.js
@@ -0,0 +1,7 @@
+import React from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App";
+document.addEventListener("turbo:load", () => {
+    const root = createRoot(document.body.appendChild(document.createElement("div")));
+    root.render(React.createElement(App, null));
+});
